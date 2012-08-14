7cbcb1d36a1f48bf80219804807774e9bd4f6f70
david@grant.org.uk <david@grant.org.uk@f934b288-bf01-11de-ba47-45a14fc341d6>
2012-08-14 19:34:51 +0000

    [maven-release-plugin]  copy for tag jscep-2.0-beta-1

---
+++ tags/jscep-2.0-beta-1/src/main/java/org/jscep/util/SignedDataUtil.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2009-2012 David Grant
+ * Copyright (c) 2010 ThruPoint Ltd
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.jscep.util;
+
+import java.security.cert.X509Certificate;
+
+import org.bouncycastle.cms.CMSException;
+import org.bouncycastle.cms.CMSSignatureAlgorithmNameGenerator;
+import org.bouncycastle.cms.CMSSignedData;
+import org.bouncycastle.cms.DefaultCMSSignatureAlgorithmNameGenerator;
+import org.bouncycastle.cms.SignerInformation;
+import org.bouncycastle.cms.SignerInformationStore;
+import org.bouncycastle.cms.SignerInformationVerifier;
+import org.bouncycastle.cms.jcajce.JcaSignerId;
+import org.bouncycastle.operator.ContentVerifierProvider;
+import org.bouncycastle.operator.DefaultSignatureAlgorithmIdentifierFinder;
+import org.bouncycastle.operator.DigestCalculatorProvider;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.SignatureAlgorithmIdentifierFinder;
+import org.bouncycastle.operator.jcajce.JcaContentVerifierProviderBuilder;
+import org.bouncycastle.operator.jcajce.JcaDigestCalculatorProviderBuilder;
+
+/**
+ * This class contains utility methods for manipulating SignedData objects.
+ * 
+ * @author David Grant
+ */
+public final class SignedDataUtil {
+    /**
+     * Private constructor to prevent instantiation.
+     */
+    private SignedDataUtil() {
+    }
+
+    /**
+     * Checks if the provided signedData was signed by the entity represented by
+     * the provided certificate.
+     * 
+     * @param sd
+     *            the signedData to verify.
+     * @param signer
+     *            the signing entity.
+     * @return <code>true</code> if the signedData was signed by the entity,
+     *         <code>false</code> otherwise.
+     */
+    public static boolean isSignedBy(CMSSignedData sd, X509Certificate signer) {
+	SignerInformationStore store = sd.getSignerInfos();
+	SignerInformation signerInfo = store.get(new JcaSignerId(signer));
+	if (signerInfo == null) {
+	    return false;
+	}
+	CMSSignatureAlgorithmNameGenerator sigNameGenerator = new DefaultCMSSignatureAlgorithmNameGenerator();
+	SignatureAlgorithmIdentifierFinder sigAlgorithmFinder = new DefaultSignatureAlgorithmIdentifierFinder();
+	ContentVerifierProvider verifierProvider;
+	try {
+	    verifierProvider = new JcaContentVerifierProviderBuilder()
+		    .build(signer);
+	} catch (OperatorCreationException e) {
+	    throw new RuntimeException(e);
+	}
+	DigestCalculatorProvider digestProvider;
+	try {
+	    digestProvider = new JcaDigestCalculatorProviderBuilder().build();
+	} catch (OperatorCreationException e1) {
+	    throw new RuntimeException(e1);
+	}
+	SignerInformationVerifier verifier = new SignerInformationVerifier(
+		sigNameGenerator, sigAlgorithmFinder, verifierProvider,
+		digestProvider);
+	try {
+	    return signerInfo.verify(verifier);
+	} catch (CMSException e) {
+	    return false;
+	}
+    }
+}
---
+++ tags/jscep-2.0-beta-1/src/test/java/org/jscep/transaction/NonceQueueTest.java
@@ -0,0 +1,31 @@
+package org.jscep.transaction;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class NonceQueueTest {
+    private static final int SIZE = 1;
+    private NonceQueue queue;
+    private Nonce nonce;
+
+    @Before
+    public void setUp() {
+	nonce = Nonce.nextNonce();
+	queue = new NonceQueue(SIZE);
+	queue.add(nonce);
+    }
+
+    @Test
+    public void testQueueContainsOriginalNonce() {
+	assertThat(queue.contains(nonce), is(true));
+    }
+
+    @Test
+    public void testQueueDoesNotContainsOriginalNonceAfterSizeExceeded() {
+	queue.add(Nonce.nextNonce());
+	assertThat(queue.contains(nonce), is(false));
+    }
+}
